5d184c74333f7f56500abf8f0f2d2229f0880042
Jordan Boston <jordandboston@gmail.com>
2018-03-11 09:16:31 -0700

    Remove header
    Style confllicts and it is not needed.
    Update favicon path.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div>
-    <Header />
-    <section class="section">
-      <div class="container">
-        <div class="columns">
-          <div class="column is-three-quarters">
-            <chess-board @moveMade="logMove($event)" />
-          </div>
-            <div class="column">
-            <sidebar :moves="moveHistory" @clearMoves="resetMoves" />
-          </div>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-three-quarters">
+          <chess-board @moveMade="logMove($event)" />
+        </div>
+          <div class="column">
+          <sidebar :moves="moveHistory" @clearMoves="resetMoves" />
         </div>
       </div>
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import ChessBoard from './components/ChessBoard'
 
 export default {
   components: {
-    Header,
     ChessBoard,
     Sidebar
   },
